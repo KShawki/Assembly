70e1dbe796828ceadd8725bc758cd09f5d31b2dd
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-01-02 16:10:06 +0200

    back

--- code.s
+++ code.s
@@ -27,7 +27,7 @@
 #   After running the program
 #   enter a positive integer and press Enter
 
-.intel_syntax noprefix  # use the intel syntax, do not prefix registers with %
+.intel_syntax noprefix  # use the intel syntax, not AT&T syntax. do not prefix registers with %
 .section .data          # memory variables
 
 input: .asciz "%d"                    # string terminated by 0 that will be used for scanf parameter
@@ -35,14 +35,13 @@ output: .asciz "The sum is: %f\n"     # string terminated by 0 that will be used
 
 n: .int 0             # the variable n which we will get from user using scanf
 s: .double 0.0        # the variable s = 1/1 + 1/2 + 1/3 + ... + 1/n that will be calculated by the program 
+                      # (1+1/1)+(2+1/4)+(3+1/9)+(4+1/16)+...+(n+1/(n^2))
 one: .double 1.0
-r: .double 1.0        # the variable r with type double and initalize 1.0
-t: .double 1.0        # the variable t with type double and initalize 1.0
+r: .double 1.0
 
 .section .text       # instructions
 .globl _main         # make _main accessible from external
 
-# Main Program Start Here ----------------------------------------------------- 
 _main:               # the label indicating the start of the program
   push OFFSET n      # push to stack the second parameter to scanf (the address of the integer variable n)
   push OFFSET input  # push to stack the first parameter to scanf
@@ -51,32 +50,21 @@ _main:               # the label indicating the start of the program
   
   mov ecx, n         # ecx <- n (the number of iterations)
 
-iteration:
-  # the following instruction to add the total sum
-  fld     qword ptr t         # push t to the floating point stack
-  fadd    qword ptr s
-  fstp    qword ptr s
-  mov     edx, dword [t]
-  mov     dword [r], edx
+loop1:
   
-  # the following instructions increase s by 1/r --------------------------------------------------
-  fld qword ptr t              # push t to the floating point stack
-  fld st(0)                    # get position 
-  fmul                         # multiplaction term => t*t
-  fstp qword ptr r             # pop the floating point stack top (1), divide it over r /
-
+  # the following 4 instructions increase s by 1/r --------------------------------------------------
   fld qword ptr one            # push 1 to the floating point stack
   fdiv qword ptr r             # pop the floating point stack top (1), divide it over r and push the result (1/r)
 
   fadd qword ptr s             # pop the floating point stack top (1/r), add it to s, and push the result (s+(1/r))
   fstp qword ptr s             # pop the floating point stack top (s+(1/r)) into the memory variable s
 
-  # the following 3 instructions increase term by 1 -------------------------------------------------
-  fld qword ptr t              # push 1 to the floating point stack
+  # the following 3 instructions increase r by 1 ----------------------------------------------------
+  fld qword ptr r              # push 1 to the floating point stack
   fadd qword ptr one           # pop the floating point stack top (1), add it to r and push the result (r+1)
-  fstp qword ptr t             # pop the floating point stack top (r+1) into the memory variable r
+  fstp qword ptr r             # pop the floating point stack top (r+1) into the memory variable r
 
-  loop iteration               # ecx -=1 , then goto iteration only if ecx is not zero
+  loop loop1         # ecx -=1 , then goto loop1 only if ecx is not zero
   
   push [s+4]         # push to stack the high 32-bits of the second parameter to printf (the double at label s)
   push s             # push to stack the low 32-bits of the second parameter to printf (the double at label s)
